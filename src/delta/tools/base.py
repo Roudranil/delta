608060a05d72a8f2ac31d46c2f959674dd2f6cbb
Roudranil <nil0auditore78@gmail.com>
2026-05-15 08:54:38 +0530

    remove all files

--- src/delta/tools/base.py
+++
@@ -1,7 +0,0 @@
-from typing import Literal
-
-from langchain_core.tools import BaseTool as LangChainBaseTool
-
-
-class BaseTool(LangChainBaseTool):
-    execution_mode: Literal["sequential", "parallel"] = "sequential"
